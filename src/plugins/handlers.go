029e742c62839b1c368619a93b8ed38beb9f54bf
aliwoto <woto@kaizoku.cyou>
2022-05-12 06:08:21 +0200

    Don't forget to call channelsPlugin.LoadAllHandlers function in plugins/handlers.go file.
    Signed-off-by: aliwoto <woto@kaizoku.cyou>

--- src/plugins/handlers.go
+++ src/plugins/handlers.go
@@ -1,10 +1,12 @@
 package plugins
 
 import (
+	"github.com/AnimeKaizoku/RepostingRobot/src/plugins/channelsPlugin"
 	"github.com/AnimeKaizoku/RepostingRobot/src/plugins/repostPlugin"
 	"github.com/PaulSonOfLars/gotgbot/v2/ext"
 )
 
 func LoadAllHandlers(d *ext.Dispatcher, triggers []rune) {
+	channelsPlugin.LoadAllHandlers(d, triggers)
 	repostPlugin.LoadAllHandlers(d, triggers)
 }
