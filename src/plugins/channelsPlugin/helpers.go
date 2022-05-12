595ace6fd50a998e9a9d05bd71cfd6ac406ab7ad
aliwoto <woto@kaizoku.cyou>
2022-05-12 05:47:27 +0200

    Fix issues in creating new handler for tmpIgnoreCommand in LoadAllHandlers function in channelsPlugin package.
    Signed-off-by: aliwoto <woto@kaizoku.cyou>

--- src/plugins/channelsPlugin/helpers.go
+++ src/plugins/channelsPlugin/helpers.go
@@ -7,7 +7,7 @@ import (
 
 func LoadAllHandlers(d *ext.Dispatcher, t []rune) {
 	registerCommand := handlers.NewCommand(registerCmd, registerCommandResponse)
-	tmpIgnoreCommand := handlers.NewCommand(tmpIgnoreCmd, registerCommandResponse)
+	tmpIgnoreCommand := handlers.NewCommand(tmpIgnoreCmd, tmpIgnoreResponse)
 
 	registerCommand.Triggers = t
 	tmpIgnoreCommand.Triggers = t
