e40ea4deea67fa81df55188e35caca83a9f41c1f
aliwoto <woto@kaizoku.cyou>
2022-05-12 04:31:38 +0000

    Fix upgrade.bat file
    Signed-off-by: aliwoto <woto@kaizoku.cyou>

--- Upgrade.bat
+++ Upgrade.bat
@@ -1,3 +1,3 @@
 @echo off
-git pull && powershell -command "Stop-service -Force -name "DominatorRobot" -ErrorAction SilentlyContinue; go build; Start-service -name "DominatorRobot""
+git pull && powershell -command "Stop-service -Force -name "RepostingRobot" -ErrorAction SilentlyContinue; go build; Start-service -name "RepostingRobot""
 :: Hail Hydra
