46687eeb481112aa366b97ca4b6216f65ae50177
aliwoto <woto@kaizoku.cyou>
2022-05-12 06:03:44 +0200

    Fix issue in BotConfig struct
    Signed-off-by: aliwoto <woto@kaizoku.cyou>

--- src/core/wotoConfig/types.go
+++ src/core/wotoConfig/types.go
@@ -2,6 +2,6 @@ package wotoConfig
 
 type BotConfig struct {
 	BotToken   string  `section:"general" key:"bot_token"`
-	ChannelIds []int64 `section:"general " key:"channel_ids"`
+	ChannelIds []int64 `section:"general" key:"channel_ids"`
 	SudoUsers  []int64 `section:"general" key:"sudo_users"`
 }
